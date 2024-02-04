566a976129bf72e408b7c36facf587b8656e1f29
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-02-04 23:35:48 +0400

    int extencion

---
+++ ConsoleApp22/IntegerExtansion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+     public static class IntegerExtansion
+    {
+        public static int NumberKub(this int number)
+        {
+            return number*number*number;
+        }
+    }
+}
---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int kub=a.NumberKub();
+            Console.WriteLine(kub);
+        }
+    }
+}
